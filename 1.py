00e04e85f52c4fdd5483eb7af4a976beb90c9c88
Potential <1317285046@qq.com>
2024-12-12 09:14:48 +0000

    kongge

--- 1.py
+++ 1.py
@@ -9,8 +9,6 @@ openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 def generate_response(input_text):
     model = ChatOpenAI(temperature=0.7, api_key=openai_api_key)
     st.info(model.invoke(input_text))
-
-
 with st.form("my_form"):
     text = st.text_area(
         "Enter text:",
